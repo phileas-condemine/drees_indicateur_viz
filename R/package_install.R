5e255828bed1939a0c0005b04519cc53d479a19f
phileas-condemine <phileas.condemine@gmail.com>
2018-01-25 18:19:25 +0100

    add installer for most packages

---
+++ R/package_install.R
@@ -0,0 +1,13 @@
+pkg_file <- read.csv("Desktop/packages.csv", sep=";")
+
+packages_list <- pkg_file[pkg_file$installed_by %in% c("custom", "notebook"),]$pkgname
+for (pkg in packages_list){
+  print(paste0("check: ",pkg))
+  if(!require(pkg,character.only = T)){
+    print(paste0("need to install: ",pkg))
+    install.packages(pkg)
+  }
+}
+
+devtools::install_github("dgrtwo/gganimate")
+devtools::install_github("hadley/ggplot2", force = TRUE)
